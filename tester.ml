cf6bf952c37fb5b0a5fc33d457d6070a8667dc6c
Nikunj Chawla <nikchawla312@gmail.com>
2020-12-15 07:32:38 -0600

    Added links to parts of documentation as necessary

--- tester.ml
+++ tester.ml
@@ -1,7 +1,7 @@
 (**
     [Tester] is a unit test framework for OCaml. It allows the easy creation of unit tests for OCaml code. It is
-    somewhat based on [OUnit2], another unit testing framework for OCaml. However, its output can be customized per test
-    case, and it is easier to see whether a case has passed or failed.
+    somewhat based on {{:https://tinyurl.com/OUnit2}OUnit2}, another unit testing framework for OCaml. However, its
+    output can be customized per test case, and it is easier to see whether a case has passed or failed.
 *)
 
 (**
@@ -19,25 +19,25 @@ type 'a t = {
 }
 
 (**
-    [test] creates an instance of the [t] record type with the given parameters
+    [test] creates an instance of the {!t} record type with the given parameters
     @param compare_fun        A function to compare the expected and actual results of the function
     @param string_of_result   A function to parse the output of the function
     @param input              The string representing the input to the function
     @param expected_result    The expected output of the function
     @param actual_result_lazy The lazily-evaluated result of actually executing the function
-    @return The [t] instance with the given values
+    @return The {!t} instance with the given values
 *)
 let test (compare_fun: 'a -> 'a -> bool) (string_of_result: 'a -> string) (input: string) (expected_result: 'a)
 (actual_result_lazy: 'a lazy_t): 'a t =
     {compare_fun; string_of_result; input; expected_result; actual_result_lazy}
 
 (**
-    [test_eq] creates an instance of the [t] record type with the given parameters and (=) as [compare_fun]
+    [test_eq] creates an instance of the {!t} record type with the given parameters and (=) as {!compare_fun}
     @param string_of_result   A function to parse the output of the function
     @param input              The string representing the input to the function
     @param expected_result    The expected output of the function
     @param actual_result_lazy The lazily-evaluated result of actually executing the function
-    @return The [t] instance with the given values
+    @return The {!t} instance with the given values
 *)
 let test_eq (string_of_result: 'a -> string) (input: string) (expected_result: 'a) (actual_result_lazy: 'a lazy_t):
 'a t = test (=) string_of_result input expected_result actual_result_lazy
@@ -47,7 +47,7 @@ let test_eq (string_of_result: 'a -> string) (input: string) (expected_result: '
     @param name       The printed name that is associated with this test case
     @param show_input True if the input string for the test should be printed
     @param show_pass  True if something should be printed if a test passes
-    @param test       The instance of [t] to be run
+    @param test       The instance of {!t} to be run
     @return True if the test passed and false if it did not
 *)
 let run_test_output (name: string) (show_input: bool) (show_pass: bool) (test: 'a t): bool =
@@ -77,20 +77,20 @@ let run_test_output (name: string) (show_input: bool) (show_pass: bool) (test: '
     @param name       The printed name that is associated with this test case
     @param show_input True if the input string for the test should be printed
     @param show_pass  True if something should be printed if a test passes
-    @param test       The instance of [t] to be run
+    @param test       The instance of {!t} to be run
     @return unit
 *)
 let run_test (name: string) (show_input: bool) (show_pass: bool) (test: 'a t): unit =
     let _ = run_test_output name show_input show_pass test in ()
 
 (**
-    [run_tests_output] runs a [list] of [t]s using [run_test] and returns a tuple containing the number of tests that
+    [run_tests_output] runs a [list] of {!t}s using {!run_test} and returns a tuple containing the number of tests that
     passed and the total number of tests
-    @param name        The name that is associated with the given [test list]
-    @param show_inputs True if the input string for each test in the [test list] should be printed
+    @param name        The name that is associated with the given [t list]
+    @param show_inputs True if the input string for each test in the [t list] should be printed
     @param show_passes True if something should be printed if a test passes for the individual test
     @param show_num    True if the number of tests passed should be shown
-    @param tests       The [list] of [t]s to run
+    @param tests       The [list] of {!t}s to run
     @return A tuple containing the number of tests that passed and the total number of tests
 *)
 let run_tests_output (name: string) (show_inputs: bool) (show_passes: bool) (show_num: bool) (tests: 'a t list):
@@ -106,12 +106,12 @@ int * int =
     (num_passed, num_tests)
 
 (**
-    [run_tests] runs a [list] of [t]s using [run_test]
+    [run_tests] runs a [list] of {!t}s using {!run_test}
     @param name        The name that is associated with the given [test list]
     @param show_inputs True if the input string for each test in the [test list] should be printed
     @param show_passes True if something should be printed if a test passes for the individual test
     @param show_num    True if the number of tests passed should be shown
-    @param tests       The [list] of [t]s to run
+    @param tests       The [list] of {!t}s to run
     @return unit
 *)
 let run_tests (name: string) (show_inputs: bool) (show_passes: bool) (show_num: bool) (tests: 'a t list): unit =
